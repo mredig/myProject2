ee4363ecb6d61adc6516af46a01c57aeb1d2cd85
Michael Redig <mredig@gmail.com>
2020-05-14 00:56:03 -0500

    (pdf page 34, beta 3) finished frontend controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,8 +1,13 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+    app.get { req -> EventLoopFuture<View> in
+		struct Context: Encodable {
+			let title: String
+			let body: String
+		}
+		let context = Context(title: "myProject - leaf", body: "Hello Leaf!")
+		return req.view.render("index", context)
     }
 
     app.get("hello") { req -> String in
