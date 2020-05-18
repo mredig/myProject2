49e814bcc085ca9c96b7f9710a63736565a98622
Michael Redig <mredig@gmail.com>
2020-05-17 19:15:42 -0500

    (pdf page 54, beta 3) finished chapter 5.

--- Sources/App/Modules/Frontend/FrontendController.swift
+++ Sources/App/Modules/Frontend/FrontendController.swift
@@ -16,7 +16,7 @@ struct FrontendController {
 							  header: "Hi there,",
 							  message: "welcome to my awesome page!",
 							  email: email)
-		return req.view.render("home", context)
+		return req.view.render("Frontend/Home", context)
 	}
 }
 
