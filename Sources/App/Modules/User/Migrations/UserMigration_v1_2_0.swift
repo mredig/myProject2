1d609e1d7a18974f334e0e2a247a60972d014241
Michael Redig <mredig@gmail.com>
2020-06-13 02:38:17 -0500

    (fix) resolve tests crashing issue

--- Sources/App/Modules/User/Migrations/UserMigration_v1_2_0.swift
+++ Sources/App/Modules/User/Migrations/UserMigration_v1_2_0.swift
@@ -3,10 +3,14 @@ import Fluent
 
 struct UserMigration_v1_2_0: Migration {
 	func prepare(on database: Database) -> EventLoopFuture<Void> {
-		database.schema(UserModel.schema)
-			.field(UserModel.FieldKeys.appleId, .string)
-			.unique(on: UserModel.FieldKeys.appleId)
-			.update()
+		database.eventLoop.flatten([
+			database.schema(UserModel.schema)
+				.field(UserModel.FieldKeys.appleId, .string)
+				.update(),
+			database.schema(UserModel.schema)
+				.unique(on: UserModel.FieldKeys.appleId)
+				.update(),
+		])
 	}
 
 	func revert(on database: Database) -> EventLoopFuture<Void> {
